8e663e4ac310b6ac4b7e965bc1ab46d02fcb5a21
Sulaiman <sloom059@gmail.com>
2026-06-06 16:17:19 +0300

    Update base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,14 +1,6 @@
-import { createClient } from '@base44/sdk';
-import { appParams } from '@/lib/app-params';
+import { createClient } from '@supabase/supabase-js'
 
-const { appId, token, functionsVersion, appBaseUrl } = appParams;
-
-//Create a client with authentication required
-export const base44 = createClient({
-  appId,
-  token,
-  functionsVersion,
-  serverUrl: '',
-  requiresAuth: false,
-  appBaseUrl
-});
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
